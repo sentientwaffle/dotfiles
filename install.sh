8b438714b92638543dce8f81f53b4d7f0fbe0777
sentientwaffle <sentientwaffle@gmail.com>
2012-05-09 12:17:46 -0700

    add template gitconfig file

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ mkdir -p "$HOME/.cmus"
 ln -s "$PWD/.cmus/current-track.sh" "$HOME/.cmus/current-track.sh"
 ln -s "$PWD/.cmus/solarized.theme" "$HOME/.cmus/solarized.theme"
 
+if [ ! -f "$HOME/.gitconfig" ];
+then
+  cp "$PWD/.gitconfig" "$HOME/.gitconfig"
+fi
+
